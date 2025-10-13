5b92acde7296150f7e9d5646d8a564c1cccc26a8
Mizichi <jbc3584@utexas.edu>
2025-10-13 14:50:06 -0500

    setting up cannon

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
+
+    //check if player collider with you is dead, if then, can't hold cannon, if player colliding with you is alive then can shoot
+    //press button to shoot for now, shoot a ball at a certain velocity
+
     // Start is called before the first frame update
     void Start()
     {
