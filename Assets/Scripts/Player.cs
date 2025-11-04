b5c05a6198accc36fed0e8cee85b0faf457fd339
Colin Day <colinday@utexas.edu>
2025-11-04 00:55:31 -0600

    Added Idle animation and Expression change
    Didn't have time to fully figure out the animation controller but its there in the models folder for anyone to mess around with

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,25 +8,35 @@ public class Player : MonoBehaviour
     public bool alive;
     private SpriteRenderer playerSprite;
 
+    [SerializeField] private SkinnedMeshRenderer faceRenderer;
+    [SerializeField] private SkinnedMeshRenderer haloRenderer;
+
+    private Material materialInstance;
+
     void Start()
     {
-        alive = alive; 
+        alive = alive;
         playerSprite = GetComponent<SpriteRenderer>();
+        materialInstance = faceRenderer.material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeExpressionOffset(0.00f);
+        haloRenderer.enabled = false;
         //if fallen off hole/get hit by enemy
         if (!alive)
         {
+            ChangeExpressionOffset(0.25f);
+            haloRenderer.enabled = true;
             Die();
         }
     }
     
     public void RespawnPlayer() //StartCoroutine(RespawnPlayer());
     {
+        ChangeExpressionOffset(0f);
         Debug.Log("Player Respawn");
         playerSprite.color = new Color(1, 0, 0, 1);
         alive = true;
@@ -36,8 +46,13 @@ public class Player : MonoBehaviour
     {
         Debug.Log("YouDied");
         //also disable button press? Probably will just have to check if other player near cannon
-        playerSprite.color = new Color(1,0,0,0.3f);//make player a ghost? 
+        playerSprite.color = new Color(1, 0, 0, 0.3f);//make player a ghost? 
 
+    }
+    
+    private void ChangeExpressionOffset(float Offset)
+    {
+        materialInstance.mainTextureOffset = new Vector2(Offset, 0f);
     }
 
 }
